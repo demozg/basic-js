12fbef6c867d5ec8aa90ade61442994742401b41
Dmitry Kuzmik <kuzmikd@mail.ru>
2023-04-13 15:35:11 +0300

    feat:add carbon and common-characters tests

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
@@ -17,9 +15,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string" || isNaN(sampleActivity) || sampleActivity < 0 || sampleActivity > 15) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+  if ([-Infinity, Infinity].includes(t))
+    return false;
+
+  return Math.ceil(t);
 }
 
 module.exports = {
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -11,9 +9,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0
+
+  if (!s1 || !s2)
+    return count;
+  for(let i = 0; i < s1.length; i++) {
+    const sameСharacters = s1[i];
+    if(s2.includes(sameСharacters)) {
+      s2 = s2.replace((sameСharacters), '')
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = {
