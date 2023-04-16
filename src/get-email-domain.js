19738d0099bd45eccc8eeb70641b6efe453d38ff
Дмитрий <kuzmikd@mail.ru>
2023-04-16 21:58:53 +0300

    feat:add get-email and hanoi-tower

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given an email address, return it's domain.
  *
@@ -10,9 +8,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let symbol = '@';
+  let indexMail = email.lastIndexOf(symbol);
+  return email.substring(indexMail + 1);
 }
 
 module.exports = {
