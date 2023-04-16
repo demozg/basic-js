b9f4869c24374301fe284becfa2bb99442f49f55
Дмитрий <kuzmikd@mail.ru>
2023-04-16 09:32:17 +0300

    feat:add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create a repeating string based on the given parameters
  *  
@@ -15,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, {
+  repeatTimes = 1,
+  separator = "+",
+  addition = "",
+  additionRepeatTimes = 1,
+  additionSeparator = "|",
+}) {
+  const strRepeater = new Array(`${addition}` ? additionRepeatTimes : 0)
+    .fill(`${addition}`)
+    .join(additionSeparator);
+  const newPhrase = new Array(+repeatTimes).fill(`${str}${strRepeater}`);
+  return newPhrase.join(separator);
 }
 
 module.exports = {
