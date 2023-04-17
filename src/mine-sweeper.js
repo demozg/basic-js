4f1db2a8e1c04d7e262ee14a4c19a7bedac883ca
Dmitry Kuzmik <kuzmikd@mail.ru>
2023-04-17 10:12:40 +0300

    feat:add mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
@@ -23,9 +21,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = new Array(matrix.length);
+  for(let i = 0; i < matrix.length; i++) {
+    const array = matrix[i];
+    let newArray = new Array(array.length);
+    for(let j = 0; j < array.length; j++) {
+      const topLeft = matrix?.[i - 1]?.[j - 1];
+      const top = matrix?.[i - 1]?.[j];
+      const topRight = matrix?.[i - 1]?.[j + 1];
+      const left = array[j - 1];
+      const right = array[j + 1];
+      const bottomLeft = matrix?.[i + 1]?.[j - 1];
+      const bottom = matrix?.[i + 1]?.[j];
+      const bottomRight = matrix?.[i + 1]?.[j + 1];
+      let sortedCount = [topLeft, top, topRight, left, right, bottomLeft, bottom, bottomRight].filter(Boolean).length;
+      newArray[j] = sortedCount;
+    }
+    result[i] = newArray;
+  }
+  return result;
 }
 
 module.exports = {
