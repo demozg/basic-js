e2e0d98b5c3f70d6ee6dca718b2c4993c8972f73
Dmitry Kuzmik <kuzmikd@mail.ru>
2023-04-17 12:05:02 +0300

    feat:add vigenere-cipher and what-season

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,13 +18,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  _codeA = 65;
+  _codeZ = 90;
+  _errorMessage = "Incorrect arguments!";
+  constructor(directMachine = true) {
+    this.directMachine = directMachine;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  reverseCipher(cipher) {
+    let reverseMachine = "";
+    for (let i = cipher.length - 1; i >= 0; i--) {
+      reverseMachine += cipher[i];
+    }
+    return reverseMachine;
+  }
+  encrypt(message, key) {
+    let encryptString = this.encryptAndDecrypt(message, key);
+    return this.directMachine
+      ? encryptString
+      : this.reverseCipher(encryptString);
+  }
+  decrypt(encryptedMessage, key) {
+    let decryptString = this.encryptAndDecrypt(encryptedMessage, key, false);
+    return this.directMachine
+      ? decryptString
+      : this.reverseCipher(decryptString);
+  }
+  encryptAndDecrypt(text, key, cipher = true) {
+    if (!text || !key) {
+      throw new Error(this._errorMessage);
+    }
+    text = text.toUpperCase();
+    key = key.padEnd(text.length, key).toUpperCase();
+    let encryptCipher = "";
+    for (let i = 0; i < text.length; i++) {
+      let letter = text[i];
+      let charCode = letter.charCodeAt();
+      if (
+        charCode < this._codeA ||
+        charCode > this._codeZ
+      ) {
+        encryptCipher += letter;
+        key = key.substring(0, i) + letter + key.substring(i);
+        continue;
+      }
+      const a = key[i].charCodeAt() - this._codeA;
+      if (cipher) {
+        const b = charCode - this._codeA;
+        const c = (b + a) % 26;
+        encryptCipher += String.fromCharCode(this._codeA + c);
+      }
+      if (!cipher) {
+        const c = charCode - this._codeA;
+        const b = (c - a + 26) % 26;
+        encryptCipher += String.fromCharCode(this._codeA + b);
+      }
+    }
+    return encryptCipher;
   }
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Extract season from given date and expose the enemy scout!
  * 
@@ -11,9 +9,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  let errorDate = "Invalid date!";
+  if (!(date instanceof Date) || date[Symbol.toStringTag] === 'Date') {
+    throw new Error(errorDate);
+  }
+  let season = date.getMonth();
+  switch (season) {
+    case 0:
+    case 1:
+    case 11:
+      return "winter";
+    case 2:
+    case 3:
+    case 4:
+      return "spring";
+    case 5:
+    case 6:
+    case 7:
+      return "summer";
+    case 8:
+    case 9:
+    case 10:
+      return "fall";
+  }
 }
 
 module.exports = {
