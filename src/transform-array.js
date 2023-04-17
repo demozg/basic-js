30f475a0f723d2cad0d80e8fc5fb39c9aa084aff
Dmitry Kuzmik <kuzmikd@mail.ru>
2023-04-17 11:10:56 +0300

    feat:add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create transformed array based on the control sequences that original
  * array contains
@@ -13,9 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr))
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  const discardNext = "--discard-next";
+  const discardPrev = "--discard-prev";
+  const doubleNext = "--double-next";
+  const doublePrev = "--double-prev";
+  let transformArray = [...arr];
+  for (let i = 0; i < transformArray.length; i++) {
+    let current = transformArray[i];
+    if (current === discardPrev) {
+      transformArray[i - 1] = null;
+      transformArray[i] = null;
+      continue;
+    }
+    if (current === doublePrev) {
+      transformArray[i] = transformArray[i - 1];
+      continue;
+    }
+    if (current === doubleNext) {
+      transformArray[i] = transformArray[i + 1];
+      continue;
+    }
+    if (current === discardNext) {
+      transformArray[i] = null;
+      transformArray[i + 1] = null;
+      continue;
+    }
+  }
+  return transformArray.filter(Boolean);
 }
 
 module.exports = {
